48f7db2d135efd86da95341f29a6d313c7f45cfa
xie-ran <e0421375@u.nus.edu>
2023-10-06 17:15:25 +0800

    Added more content to the dashboard

--- react-flask-app/src/Components/CompanyProfile/CompanyProfile.js
+++ react-flask-app/src/Components/CompanyProfile/CompanyProfile.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
-import { Pie, Line } from 'react-chartjs-2';
-import { Chart, PieController, ArcElement, CategoryScale, Tooltip, Legend, LineController, LineElement, PointElement, LinearScale } from 'chart.js';
+import { Pie, Line, Bar, Doughnut } from 'react-chartjs-2';
+import { Chart, PieController, ArcElement, CategoryScale, Tooltip, Legend, LineController, LineElement, PointElement, LinearScale, BarController, BarElement } from 'chart.js';
+import './CompanyProfile.css';
 
-// Register the necessary components for Pie and Line charts
-Chart.register(PieController, ArcElement, CategoryScale, Tooltip, Legend, LineController, LineElement, PointElement, LinearScale);
+
+// Register the necessary components for Pie, Line, and Bar charts
+Chart.register(PieController, ArcElement, CategoryScale, Tooltip, Legend, LineController, LineElement, PointElement, LinearScale, BarController, BarElement);
 
 function CompanyProfile() {
-    // Dummy Data
+    // Dummy Data for added visualizations (update these with real data when available)
     const riskData = {
         labels: ['Operational', 'Legal', 'Loan', 'Others'],
         datasets: [{
@@ -27,30 +29,71 @@ function CompanyProfile() {
         }]
     };
 
+    const esgData = {
+        labels: ['Environmental', 'Social', 'Governance'],
+        datasets: [{
+            data: [40, 30, 30],
+            backgroundColor: ['#33A02C', '#1F78B4', '#E31A1C']
+        }]
+    };
+
+    const analystRatingsData = {
+        labels: ['Buy', 'Hold', 'Sell'],
+        datasets: [{
+            data: [10, 5, 2],
+            backgroundColor: ['#5DADE2', '#F4D03F', '#E74C3C']
+        }]
+    };
+
+    // Placeholder for news and commentaries
+    const newsItems = ["News Item 1", "News Item 2", "..."];
+    const analystComments = ["Analyst Comment 1", "Analyst Comment 2", "..."];
+
     return (
         <Container className="mt-5">
             <h2>Investor's View - Company Dashboard</h2>
-            <Row className="mt-4">
-                <Col md={6}>
-                    <Card className="mb-4">
-                        <Card.Body>
-                            <Card.Title>Risk Assessment</Card.Title>
+            <div className="gridContainer">
+            <Card className="mb-4 riskCard">
+                    <Card.Body>
+                        <Card.Title>Risk Assessment</Card.Title>
+                        <div className="chartContainer">
                             <Pie data={riskData} />
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col md={6}>
-                    <Card className="mb-4">
-                        <Card.Body>
-                            <Card.Title>Stock Price Trend</Card.Title>
+                        </div>
+                    </Card.Body>
+                </Card>
+
+                <Card className="mb-4 stockCard">
+                    <Card.Body>
+                        <Card.Title>Stock Price Trend</Card.Title>
+                        <div className="chartContainer">
                             <Line data={stockData} />
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            {/* ... other components such as company valuation metrics, financial health, etc. */}
+                        </div>
+                    </Card.Body>
+                </Card>
+
+                <Card className="mb-4 esgCard">
+                    <Card.Body>
+                        <Card.Title>ESG Metrics</Card.Title>
+                        <div className="chartContainer">
+                            <Doughnut data={esgData} />
+                        </div>
+                    </Card.Body>
+                </Card>
+
+    
+                <Card className="mb-4 newsCard">
+                    <Card.Body>
+                        <Card.Title>Company News</Card.Title>
+                        <ul>{newsItems.map((news, index) => <li key={index}>{news}</li>)}</ul>
+                        <hr />
+                        <Card.Title>Analyst Commentary</Card.Title>
+                        <ul>{analystComments.map((comment, index) => <li key={index}>{comment}</li>)}</ul>
+                    </Card.Body>
+                </Card>
+            </div>
         </Container>
     );
+    
 }
 
 export default CompanyProfile;
